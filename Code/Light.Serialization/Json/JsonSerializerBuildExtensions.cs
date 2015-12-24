6b42831f83a9d0addb998ab46e1b68d39a72c996
Kenny Pflug <kenny.pflug@live.de>
2015-12-24 09:36:42 +0100

    extended JsonSerializerBuilder and added static methods to easily create the default lists of primitve formatters and and writer instructors

---
+++ Code/Light.Serialization/Json/JsonSerializerBuildExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Light.Serialization.Json.ComplexTypeDecomposition;
+using Light.Serialization.Json.PrimitiveTypeFormatters;
+using Light.Serialization.Json.WriterInstructors;
+
+namespace Light.Serialization.Json
+{
+    public static class JsonSerializerBuildExtensions
+    {
+        public static IList<IPrimitiveTypeFormatter> AddDefaultPrimitiveTypeFormatters(this IList<IPrimitiveTypeFormatter> targetList, ICharacterEscaper characterEscaper)
+        {
+            if (targetList == null) throw new ArgumentNullException(nameof(targetList));
+
+            targetList.Add(new ToStringPrimitiveTypeFormatter<int>());
+            targetList.Add(new StringFormatter(characterEscaper));
+            targetList.Add(new DoubleFormatter());
+            targetList.Add(new ToStringWithQuotationMarksFormatter<Guid>());
+            targetList.Add(new BooleanFormatter());
+            targetList.Add(new DecimalFormatter());
+            targetList.Add(new ToStringPrimitiveTypeFormatter<long>());
+            targetList.Add(new FloatFormatter());
+            targetList.Add(new CharFormatter(characterEscaper));
+            targetList.Add(new ToStringPrimitiveTypeFormatter<short>());
+            targetList.Add(new ToStringPrimitiveTypeFormatter<byte>());
+            targetList.Add(new ToStringPrimitiveTypeFormatter<uint>());
+            targetList.Add(new ToStringPrimitiveTypeFormatter<ulong>());
+            targetList.Add(new ToStringPrimitiveTypeFormatter<ushort>());
+            targetList.Add(new ToStringPrimitiveTypeFormatter<sbyte>());
+
+            return targetList;
+        }
+
+        public static IList<IJsonWriterInstructor> AddDefaultWriterInstructors(this IList<IJsonWriterInstructor> targetList,
+                                                                               IDictionary<Type, IPrimitiveTypeFormatter> primitiveTypeToFormattersMapping,
+                                                                               IReadableValuesTypeAnalyzer readableValuesTypeAnalyzer)
+        {
+            if (targetList == null) throw new ArgumentNullException(nameof(targetList));
+
+            targetList.Add(new PrimitiveWriterInstructor(primitiveTypeToFormattersMapping));
+            targetList.Add(new EnumerationToStringInstructor());
+            targetList.Add(new DictionaryInstructor(primitiveTypeToFormattersMapping));
+            targetList.Add(new CollectionInstructor());
+            targetList.Add(new ComplexWriterInstructor(readableValuesTypeAnalyzer));
+
+            return targetList;
+        }
+    }
+}
--- Code/Light.Serialization/Json/JsonSerializerBuilder.cs
+++ Code/Light.Serialization/Json/JsonSerializerBuilder.cs
@@ -1,54 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Light.Serialization.Json.ComplexTypeDecomposition;
 using Light.Serialization.Json.PrimitiveTypeFormatters;
-using Light.Serialization.Json.WriterInstructors;
 
 namespace Light.Serialization.Json
 {
     public class JsonSerializerBuilder
     {
-        private readonly IList<IJsonWriterInstructor> _typeSerializers;
-        private readonly IJsonWriterFactory _writerFactory;
+        private IJsonWriterFactory _writerFactory;
+        private IList<IJsonWriterInstructor> _writerInstructors;
 
         public JsonSerializerBuilder()
         {
             var characterEscaper = new DefaultCharacterEscaper();
-            var primitiveTypeToFormattersMapping = new IPrimitiveTypeFormatter[]
-                                                   {
-                                                       new ToStringPrimitiveTypeFormatter<int>(),
-                                                       new StringFormatter(characterEscaper),
-                                                       new DoubleFormatter(),
-                                                       new ToStringWithQuotationMarksFormatter<Guid>(),
-                                                       new BooleanFormatter(),
-                                                       new DecimalFormatter(),
-                                                       new ToStringPrimitiveTypeFormatter<long>(),
-                                                       new FloatFormatter(),
-                                                       new CharFormatter(characterEscaper),
-                                                       new ToStringPrimitiveTypeFormatter<short>(),
-                                                       new ToStringPrimitiveTypeFormatter<byte>(),
-                                                       new ToStringPrimitiveTypeFormatter<uint>(),
-                                                       new ToStringPrimitiveTypeFormatter<ulong>(),
-                                                       new ToStringPrimitiveTypeFormatter<ushort>(),
-                                                       new ToStringPrimitiveTypeFormatter<sbyte>()
-                                                   }.ToDictionary(f => f.TargetType);
+            var primitiveTypeToFormattersMapping = new List<IPrimitiveTypeFormatter>().AddDefaultPrimitiveTypeFormatters(characterEscaper)
+                                                                                      .ToDictionary(f => f.TargetType);
 
-            _typeSerializers = new List<IJsonWriterInstructor>
-                               {
-                                   new PrimitiveWriterInstructor(primitiveTypeToFormattersMapping),
-                                   new EnumerationToStringInstructor(),
-                                   new DictionaryInstructor(primitiveTypeToFormattersMapping),
-                                   new CollectionInstructor(),
-                                   new ComplexWriterInstructor(new PublicPropertiesAndFieldsAnalyzer())
-                               };
+            var publicPropertiesAndFieldsAnalyzer = new PublicPropertiesAndFieldsAnalyzer();
+            _writerInstructors = new List<IJsonWriterInstructor>().AddDefaultWriterInstructors(primitiveTypeToFormattersMapping,
+                                                                                               publicPropertiesAndFieldsAnalyzer);
 
             _writerFactory = new JsonWriterFactory();
         }
 
+        public JsonSerializerBuilder WithWriterInstructors(IList<IJsonWriterInstructor> writerInstructors)
+        {
+            _writerInstructors = writerInstructors;
+            return this;
+        }
+
+        public JsonSerializerBuilder WithWriterFactory(IJsonWriterFactory writerFactory)
+        {
+            _writerFactory = writerFactory;
+            return this;
+        }
+
         public ISerializer Build()
         {
-            return new JsonSerializer(_typeSerializers, _writerFactory);
+            return new JsonSerializer(_writerInstructors, _writerFactory);
         }
     }
 }
