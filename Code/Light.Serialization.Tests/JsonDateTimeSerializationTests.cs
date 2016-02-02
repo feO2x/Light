8f83fefe9b4bff89e18010c4f9b3d9bbad0d7fcf
Kenny Pflug <kenny.pflug@live.de>
2016-02-02 11:41:47 +0100

    started JSON DateTime serialization tests

---
+++ Code/Light.Serialization.Tests/JsonDateTimeSerializationTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using TestData = System.Collections.Generic.IEnumerable<object[]>;
+
+namespace Light.Serialization.Tests
+{
+    public sealed class JsonDateTimeSerializationTests : BaseDefaultJsonSerializerTest
+    {
+        public static readonly TestData Utc1TestData =
+            new[]
+            {
+                new object[] { new DateTime(2016, 2, 2, 0, 0, 0, DateTimeKind.Utc), "\"2016-02-02T00:00:00Z\"" }
+            };
+
+        //https://en.wikipedia.org/wiki/ISO_8601
+        [Theory(DisplayName = "UTC date times are serialized according to the ISO 8601 specification")]
+        [MemberData(nameof(Utc1TestData))]
+        public void Utc1(DateTime dateTime, string expectedJson)
+        {
+            CompareJsonToExpected(dateTime, expectedJson);
+        }
+    }
+}
