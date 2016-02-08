63b746645056a76fb424f00dd058b381018a905d
Markus Webert <markuswebert@gmx.de>
2016-02-08 10:52:05 +0100

    fixed wrong spelling in EnumValueNotDefinedException

--- Code/Light.GuardClauses/Exceptions/EnumValueNotDefinedException.cs
+++ Code/Light.GuardClauses/Exceptions/EnumValueNotDefinedException.cs
@@ -5,7 +5,7 @@ namespace Light.GuardClauses.Exceptions
     public class EnumValueNotDefinedException : ArgumentException
     {
         public EnumValueNotDefinedException(string parameterName, object actualValue, Type enumType, Exception innerException = null)
-            : base($"{parameterName}should be a value of enum {enumType.FullName}, but you specified {actualValue}.", parameterName, innerException)
+            : base($"{parameterName} should be a value of enum {enumType.FullName}, but you specified {actualValue}.", parameterName, innerException)
         {
             
         }
