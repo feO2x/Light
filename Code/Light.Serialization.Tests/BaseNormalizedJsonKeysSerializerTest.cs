8fb6c54b138d1c6a5a31c94225bd705283dd1740
Markus Webert <markuswebert@gmx.de>
2016-02-10 18:08:00 +0100

    added testclass for normalizedJsonKeysSerializerTests and added tests for normalizing json keys

---
+++ Code/Light.Serialization.Tests/BaseNormalizedJsonKeysSerializerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Light.Serialization.Json;
+using Light.Serialization.Json.LowLevelWriting;
+
+namespace Light.Serialization.Tests
+{
+    public abstract class BaseNormalizedJsonKeysSerializerTest
+    {
+        protected readonly ISerializer JsonSerializer;
+
+        protected BaseNormalizedJsonKeysSerializerTest()
+        {
+            JsonSerializer = new JsonSerializerBuilder().WithWriterFactory(new JsonWriterNormalizedKeysDecoratorFactory())
+                                                        .Build();
+        }
+
+        protected void CompareJsonToExpected<T>(T value, string expected)
+        {
+            var json = JsonSerializer.Serialize(value);
+
+            json.Should().Be(expected);
+        }
+    }
+}
---
+++ Code/Light.Serialization.Tests/JsonNormalizedKeysTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Light.Serialization.Json;
+using Xunit;
+
+namespace Light.Serialization.Tests
+{
+    public class JsonNormalizedKeysTests : BaseNormalizedJsonKeysSerializerTest
+    {
+        [Fact(DisplayName = "Json Keys will be normalized correctly with the JsonNormalizedKeyWriter.")]
+        public void CustomObjectNormalizeJsonKeys()
+        {
+            var moreComplexDummyObject = new MoreComplexDummyClass("PrivateFieldValue", 11, new DateTime(2016, 2, 10), 42.0, "PublicStringProperty", "publicStringPropertyTwo");
+            var serializer = new JsonSerializerBuilder().WithRuleFor<JsonCustomRuleSerializationTest.MoreComplexDummyClass>(r => r.IgnoreAll()
+                                                                                                  .ButProperty(o => o.PublicStringProperty)
+                                                                                                  .AndField(o => o.PublicField))
+                                                        .Build();
+
+            var json = serializer.Serialize(moreComplexDummyObject);
+
+            json.Should().Be("{\"publicProperty\":\"2016-02-10\",\"publicDoubleProperty\":42.0,\"publicStringProperty\":\"PublicStringProperty\",\"publicStringPropertyTwo\":\"publicStringPropertyTwo\",\"publicField\":11}");
+        }
+
+        public class MoreComplexDummyClass
+        {
+            public static int PublicStaticField = 0;
+            // ReSharper disable once NotAccessedField.Local
+            private string _privateField;
+            public int PublicField;
+
+            public MoreComplexDummyClass(string privateFieldValue, int publicFieldValue, DateTime publicPropertyValue, double publicDoubleProperty, string publicStringProperty, string publicStringPropertyTwo)
+            {
+                _privateField = privateFieldValue;
+                PublicField = publicFieldValue;
+                PublicProperty = publicPropertyValue;
+                PublicDoubleProperty = publicDoubleProperty;
+                PublicStringProperty = publicStringProperty;
+                PublicStringPropertyTwo = publicStringPropertyTwo;
+            }
+
+            public DateTime PublicProperty { get; }
+            public double PublicDoubleProperty { get; set; }
+            public string PublicStringProperty { get; set; }
+            public string PublicStringPropertyTwo { get; set; }
+        }
+    }
+}
