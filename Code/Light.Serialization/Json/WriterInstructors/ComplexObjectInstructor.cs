7e298f0ecbdf7e9a4143384ec1871f51cbb43cbe
Kenny Pflug <kenny.pflug@live.de>
2016-02-11 11:20:22 +0100

    refactored duplicated code in ComplexObjectInstructor and CustomRuleInstructor

--- Code/Light.Serialization/Json/WriterInstructors/ComplexObjectInstructor.cs
+++ Code/Light.Serialization/Json/WriterInstructors/ComplexObjectInstructor.cs
@@ -23,31 +23,7 @@ namespace Light.Serialization.Json.WriterInstructors
         public void Serialize(JsonSerializationContext serializationContext)
         {
             var valueProviders = _typeAnalyzer.AnalyzeType(serializationContext.ActualType);
-            var writer = serializationContext.Writer;
-            
-            writer.BeginObject();
-            for (var i = 0; i < valueProviders.Count; i++)
-            {
-                var valueProvider = valueProviders[i];
-                var childObject = valueProvider.GetValue(serializationContext.ObjectToBeSerialized);
-
-                if (childObject == null)
-                {
-                    writer.WriteKey(valueProvider.Name);
-                    writer.WriteNull();
-                }
-                else
-                {
-                    var childObjectType = childObject.GetType(); // TODO: This might end up in an endless loop if e.g. a property returns a reference to the object itself. Can be solved with a dictionary that contains all objects being serialized (what I wanted to integrate in the first place).
-                    writer.WriteKey(valueProvider.Name);
-                    serializationContext.SerializeChildObject(childObject, childObjectType, valueProvider.ReferenceType);
-                }
-
-                if (i < valueProviders.Count - 1)
-                    writer.WriteDelimiter();
-            }
-
-            writer.EndObject();
+            ComplexObjectWriting.WriteValues(serializationContext, valueProviders);
         }
     }
 }
---
+++ Code/Light.Serialization/Json/WriterInstructors/ComplexObjectWriting.cs
@@ -0,0 +1,36 @@
+﻿using Light.Serialization.Json.ComplexTypeDecomposition;
+using System.Collections.Generic;
+using Light.GuardClauses;
+
+namespace Light.Serialization.Json.WriterInstructors
+{
+    public class ComplexObjectWriting
+    {
+        public static void WriteValues(JsonSerializationContext context, IList<IValueProvider> valueProvidersForComplexObject)
+        {
+            valueProvidersForComplexObject.MustNotBeNull(nameof(valueProvidersForComplexObject));
+
+            var writer = context.Writer;
+            writer.BeginObject();
+            for (var i = 0; i < valueProvidersForComplexObject.Count; i++)
+            {
+                var valueProvider = valueProvidersForComplexObject[i];
+                var childValue = valueProvider.GetValue(context.ObjectToBeSerialized);
+
+                writer.WriteKey(valueProvider.Name);
+                if (childValue == null)
+                    writer.WriteNull();
+                else
+                {
+                    // TODO: This might end up in an endless loop if e.g. a property returns a reference to the object itself. Can be solved with a dictionary that contains all objects being serialized (what I wanted to integrate in the first place).
+                    var childValueType = childValue.GetType();
+                    context.SerializeChildObject(childValue, childValueType, valueProvider.ReferenceType);
+                }
+
+                if (i < valueProvidersForComplexObject.Count - 1)
+                    writer.WriteDelimiter();
+            }
+            writer.EndObject();
+        }
+    }
+}
