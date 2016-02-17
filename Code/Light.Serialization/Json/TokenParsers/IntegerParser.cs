0e78d414971f1fe7405b2d2a56bd38e88044aa22
Markus Webert <markuswebert@gmx.de>
2016-02-17 09:11:25 +0100

    added instance variables and isSuitableFor to template

--- Code/Light.Serialization/Json/TokenParsers/IntegerParser.cs
+++ Code/Light.Serialization/Json/TokenParsers/IntegerParser.cs
@@ -1,3 +1,38 @@
-﻿
-				public class ShortParser {}
+﻿using System;
+
+namespace Light.Serialization.Json.TokenParsers
+{
+
+
+	public class ShortParser : IJsonTokenParser
+	{
+		private readonly Type _integerType = typeof (short);
+		
+        public const char DecimalPointCharacter = '.';
+        public const char NegativeSign = '-';
+
+        public const string MaxAsString = "1 ";
+        public const string MinAsString = "1 ";
+
+		public bool IsSuitableFor(JsonToken token, Type requestedType)
+        {
+            return (token.JsonType == JsonTokenType.IntegerNumber || token.JsonType == JsonTokenType.FloatingPointNumber) && requestedType == _integerType;
+        }
+	}
 			
+	public class UIntParser : IJsonTokenParser
+	{
+		private readonly Type _integerType = typeof (uint);
+		
+        public const char DecimalPointCharacter = '.';
+        public const char NegativeSign = '';
+
+        public const string MaxAsString = "1 ";
+        public const string MinAsString = "1 ";
+
+		public bool IsSuitableFor(JsonToken token, Type requestedType)
+        {
+            return (token.JsonType == JsonTokenType.IntegerNumber || token.JsonType == JsonTokenType.FloatingPointNumber) && requestedType == _integerType;
+        }
+	}
+			}
