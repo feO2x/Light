a827395c3f008997675f87b50861cb9b83e19da3
Markus Webert <markuswebert@gmx.de>
2016-03-08 17:37:53 +0100

    added base classes for using BuilderTests with UnityContainer; rearranged JsonTokenParsers in UnityContainerDeserialization (like the parsers in the Builder)

---
+++ Code/Light.Serialization.UnityContainerIntegration.Tests/BaseDefaultJsonDeserializationTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Domain;
+using FluentAssertions;
+using Light.Serialization.Json;
+using Light.Serialization.UnityContainerIntegration;
+using Microsoft.Practices.Unity;
+
+// ReSharper disable once CheckNamespace
+namespace Light.Serialization.Tests
+{
+    public abstract class BaseDefaultJsonDeserializationTest
+    {
+        public static void CompareDeserializedJsonToExpected<T>(string json, T expected)
+        {
+            var diContainer = new UnityContainer().RegisterDefaultDeserializationTypes();
+            var testTarget = diContainer.Resolve<IDeserializer>();
+            var actual = testTarget.Deserialize<T>(json);
+            actual.Should().Be(expected);
+        }
+
+        public static T GetDeserializedJson<T>(string json)
+        {
+            var diContainer = new UnityContainer().RegisterDefaultDeserializationTypes();
+            var testTarget = diContainer.Resolve<IDeserializer>();
+            return testTarget.Deserialize<T>(json);
+        }
+
+        public static object GetDeserializedJson(string json, Type requestedType)
+        {
+            var diContainer = new UnityContainer().RegisterDefaultDeserializationTypes();
+            var testTarget = diContainer.Resolve<IDeserializer>();
+            return testTarget.Deserialize(json, requestedType);
+        }
+
+        public static void CheckDeserializerThrowsExceptionWithMessageContaining<T>(string json, string partOfExceptionMessage)
+        {
+            var diContainer = new UnityContainer().RegisterDefaultDeserializationTypes();
+            var testTarget = diContainer.Resolve<IDeserializer>();
+            Action act = () => testTarget.Deserialize<T>(json);
+            act.ShouldThrow<DeserializationException>().And.Message.Should().Contain(partOfExceptionMessage);
+        }
+
+        public static void CheckDeserializerThrowsExceptionWithMessage<T>(string json, string exceptionMessage)
+        {
+            var diContainer = new UnityContainer().RegisterDefaultDeserializationTypes();
+            var testTarget = diContainer.Resolve<IDeserializer>();
+            Action act = () => testTarget.Deserialize<T>(json);
+            act.ShouldThrow<DeserializationException>().And.Message.Should().Be(exceptionMessage);
+        }
+    }
+}
---
+++ Code/Light.Serialization.UnityContainerIntegration.Tests/BaseDefaultJsonSerializerTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Light.Serialization.Json;
+using Light.Serialization.UnityContainerIntegration;
+using Microsoft.Practices.Unity;
+
+// ReSharper disable once CheckNamespace
+namespace Light.Serialization.Tests
+{
+    public abstract class BaseDefaultJsonSerializerTest
+    {
+        protected ISerializer JsonSerializer;
+
+        protected BaseDefaultJsonSerializerTest()
+        {
+            JsonSerializer = new UnityContainer().RegisterDefaultSerializationTypes().Resolve<ISerializer>();
+        }
+
+        protected void CompareJsonToExpected<T>(T value, string expected)
+        {
+            var json = JsonSerializer.Serialize(value);
+
+            json.Should().Be(expected);
+        }
+    }
+}
