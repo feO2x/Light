7e203f213ef2fb26ecc2fb99284eed4028fff689
Markus Webert <markuswebert@gmx.de>
2016-03-10 16:06:59 +0400

    added struct for jsonToken and type combination; added stub for tokenParser cache

---
+++ Code/Light.Serialization/Json/Caching/JsonTokenTypeCombination.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Light.GuardClauses;
+
+namespace Light.Serialization.Json.Caching
+{
+    public struct JsonTokenTypeCombination
+    {
+        public JsonTokenTypeCombination(JsonToken jsonToken, Type type)
+        {
+            jsonToken.MustNotBeNull(nameof(jsonToken));
+            type.MustNotBeNull(nameof(type));
+
+            JsonToken = jsonToken;
+            Type = type;
+        }
+
+        public JsonToken JsonToken { get; }
+        public Type Type { get; }
+    }
+}
---
+++ Code/Light.Serialization/Json/Caching/TokenParserCacheDecorator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Light.GuardClauses;
+
+namespace Light.Serialization.Json.Caching
+{
+    public class TokenParserCacheDecorator
+    {
+        private IList<JsonTokenTypeCombination> _jsonTokenTypeCombinationCacheBlackList; 
+
+        public TokenParserCacheDecorator(IList<JsonTokenTypeCombination> jsonTokenTypeCombinationCacheBlackList)
+        {
+            jsonTokenTypeCombinationCacheBlackList.MustNotBeNull(nameof(jsonTokenTypeCombinationCacheBlackList));
+
+            _jsonTokenTypeCombinationCacheBlackList = jsonTokenTypeCombinationCacheBlackList;
+        }
+
+        public bool CheckJsonTokenTypeForBlacklist(JsonToken jsonToken, Type type)
+        {
+            jsonToken.MustNotBeNull(nameof(jsonToken));
+            type.MustNotBeNull(nameof(type));
+            CheckJsonTokenTypeCombinationForBlacklist(new JsonTokenTypeCombination(jsonToken, type));
+        }
+
+        public bool CheckJsonTokenTypeCombinationForBlacklist(JsonTokenTypeCombination jsonTokenTypeCombination)
+        {
+            
+        }
+    }
+}
