7b1f6907da15d0c9dd41a17db6bf0774c5e41df8
Kenny Pflug <kenny.pflug@live.de>
2016-03-14 11:44:43 +0100

    extended SimpleNameToTypeMapping to implement ITypeToNameMapping

--- Code/Light.Serialization/Json/TypeNaming/SimpleNameToTypeMapping.cs
+++ Code/Light.Serialization/Json/TypeNaming/SimpleNameToTypeMapping.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using Light.GuardClauses;
 
 namespace Light.Serialization.Json.TypeNaming
 {
-    public sealed class SimpleNameToTypeMapping : INameToTypeMapping
+    public sealed class SimpleNameToTypeMapping : INameToTypeMapping, ITypeToNameMapping
     {
         public Type Map(string typeName)
         {
             return Type.GetType(typeName);
+        }
+
+        public string Map(Type type)
+        {
+            type.MustNotBeNull(nameof(type));
+
+            return type.FullName;
         }
     }
 }
