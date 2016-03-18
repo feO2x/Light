228ac6728612cec23905758f89e6f8233664af75
Kenny Pflug <kenny.pflug@live.de>
2016-03-18 07:49:24 +0100

    refactored MustNotBeIn, added comments and tests

--- Code/Light.GuardClauses/ComparableAssertions.cs
+++ Code/Light.GuardClauses/ComparableAssertions.cs
@@ -152,14 +152,35 @@ namespace Light.GuardClauses
                 throw exception ?? new ArgumentOutOfRangeException(parameterName, parameter, message ?? $"{parameterName ?? "The value"} must be between {range.From} ({fromBoundaryKind}) and {range.To} ({toBoundaryKind}), but you specified {parameter}.");
         }
 
+        /// <summary>
+        ///     Ensures that <paramref name="parameter" /> is not within the specified <paramref name="range" />, or otherwise
+        ///     throws an <see cref="ArgumentOutOfRangeException" />.
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter to be checked.</typeparam>
+        /// <param name="parameter">The parameter to be checked.</param>
+        /// <param name="range">The range that <paramref name="parameter" /> must not be in between.</param>
+        /// <param name="parameterName">The name of the parameter (optional).</param>
+        /// <param name="message">
+        ///     The message that should be injected into the <see cref="ArgumentOutOfRangeException" />
+        ///     (optional).
+        /// </param>
+        /// <param name="exception">
+        ///     The exception that is thrown when the specified <paramref name="parameter" /> is within
+        ///     <paramref name="range" /> (optional). Please note that <paramref name="message" /> and
+        ///     <paramref name="parameterName" /> are both ignored when you specify exception.
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Thrown when the specified <paramref name="parameter" /> is within
+        ///     <paramref name="range" /> and no <paramref name="exception" /> is specified.
+        /// </exception>
         [Conditional(Check.CompileAssertionsSymbol)]
-        public static void MustNotBeIn<T>(this T parameter, Range<T> range, string parameterName) where T : IComparable<T>
+        public static void MustNotBeIn<T>(this T parameter, Range<T> range, string parameterName = null, string message = null, Exception exception = null) where T : IComparable<T>
         {
             var fromBoundaryKind = range.IsFromInclusive ? "inclusive" : "exclusive";
             var toBoundaryKind = range.IsToInclusive ? "inclusive" : "exclusive";
 
             if (range.IsValueWithinRange(parameter))
-                throw new ArgumentOutOfRangeException(parameterName, parameter, $"{parameterName} must not be between {range.From} ({fromBoundaryKind}) and {range.To} ({toBoundaryKind}), but you specified {parameter}.");
+                throw exception ?? new ArgumentOutOfRangeException(parameterName, parameter, message ?? $"{parameterName ?? "The value"} must not be between {range.From} ({fromBoundaryKind}) and {range.To} ({toBoundaryKind}), but you specified {parameter}.");
         }
     }
 }
