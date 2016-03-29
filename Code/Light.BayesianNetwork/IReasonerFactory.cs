1448ce4df42a2c11d45759632ab42c8bec324f46
Markus Webert <markuswebert@gmx.de>
2016-03-29 17:19:07 +0400

    updated guardClauses; added IReasonerFactory

---
+++ Code/Light.BayesianNetwork/IReasonerFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Light.BayesianNetwork
+{
+    public interface IReasonerFactory
+    {
+        IReasoner Create(IReadOnlyList<RandomVariableNode> networkNodes, IProbabilityCalculator probabilityCalculator);
+    }
+}
