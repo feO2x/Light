c29409855406b8dd66f3b2cf1a7e71027689bbec
Markus Webert <markuswebert@gmx.de>
2016-04-05 00:24:00 +0400

    added builder for naiveBayes bayesianNetwork

---
+++ Code/Light.BayesianNetwork/NaiveBayes/NaiveBayesBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Light.BayesianNetwork.Calculators;
+using Light.GuardClauses;
+
+namespace Light.BayesianNetwork.NaiveBayes
+{
+    public class NaiveBayesBuilder
+    {
+        private Guid _guid = Guid.NewGuid();
+        private ICollectionFactory _collectionFactory = new DefaultCollectionFactory();
+        private IReasonerFactory _reasonerFactory = new NaiveBayesReasonerFactory();
+        private IProbabilityCalculatorFactory _probabilityCalculatorFactory = new DotNetProbabilityCalculatorFactory();
+
+        public NaiveBayesBuilder WithGuid(Guid guid)
+        {
+            _guid = guid;
+            return this;
+        }
+
+        public NaiveBayesBuilder WithCollectionFactory(ICollectionFactory collectionFactory)
+        {
+            collectionFactory.MustNotBeNull(nameof(collectionFactory));
+
+            _collectionFactory = collectionFactory;
+            return this;
+        }
+
+        public NaiveBayesBuilder WithReasonerFactory(IReasonerFactory reasonerFactory)
+        {
+            reasonerFactory.MustNotBeNull(nameof(reasonerFactory));
+
+            _reasonerFactory = reasonerFactory;
+            return this;
+        }
+
+        public NaiveBayesBuilder WithProbabilityCalculatorFactory(
+            IProbabilityCalculatorFactory probabilityCalculatorFactory)
+        {
+            probabilityCalculatorFactory.MustNotBeNull(nameof(probabilityCalculatorFactory));
+
+            _probabilityCalculatorFactory = probabilityCalculatorFactory;
+            return this;
+        }
+
+        public BayesianNetwork Build()
+        {
+            return new BayesianNetwork(_guid, _collectionFactory, _reasonerFactory, _probabilityCalculatorFactory);
+        }
+    }
+}
