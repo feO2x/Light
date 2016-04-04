16ff3aa201a6f37d4d3c4820c8af798a51ca967a
Markus Webert <markuswebert@gmx.de>
2016-04-05 00:40:17 +0400

    added simple test that builds up a NBN and verifies that new NW has no nodes

---
+++ Code/Light.BayesianNetwork.Tests/NaiveBayesNetworkTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Light.BayesianNetwork.NaiveBayes;
+using Xunit;
+
+namespace Light.BayesianNetwork.Tests
+{
+    public sealed class NaiveBayesNetworkTests
+    {
+        [Fact(DisplayName = "A new naive bayes network should have zero nodes.")]
+        // ReSharper disable once InconsistentNaming
+        public void NaiveBNWithZeroNodes()
+        {
+            var network = new NaiveBayesBuilder().Build();
+            network.Nodes.Count.Should().Be(0);
+        }
+    }
+}
